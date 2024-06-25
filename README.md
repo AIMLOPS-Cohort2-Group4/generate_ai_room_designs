15d4fbfea659eae67f94840721cb8afdb5863e1b
SamanK <saman.khan.stats@gmail.com>
2024-06-25 10:49:04 +0530

    added information from room to item and category dictionary pickle to the annotation_ikea.json

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-# generate_ai_room_designs
+## Generative AI based Interior Designs
 
+Interior decoration using generative AI addresses the challenges faced by individuals in personalizing and visualising interior design choices efficiently and effectively. This project proposes the use of Generative AI to create a tool that can generate personalized design recommendations based on user inputs, offer real time visualization of these designs in the user’s own space thereby simplifying the interior decoration process and enhancing user satisfaction
 
-final annotations file can be found at :
-with the name: annotations_main.json
+### Data Source
 
-export PROJECT_ROOT_PATH=”/Users/samankhan/Documents/capstone_project/Github_project/generate_ai_room_designs/”
+Dataset was collected from IKEA.com website for the purpose of building the project.
+It consists of :
+* 2193 object (product) photos.
+* 298 context (room scene) photos in which those objects appear.
+* Text descriptions for products.
+* Ground truth information on which items appear in which rooms.
+We group together objects of the same category (chair, table, sofa, etc).
 
+![alt text](https://github.com/IvonaTau/ikea/blob/master/dataset_description.png)
 
-## Data Preparation:
--- filename_key missing file extension
+### setting up the virtual environment
+
+To setup the virtual environment run the below commands:
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+### Data Preprocessing
+
+Used the pickle file available at open source platform and converted the same to readable and informative json using the method /caption_generation_static.ipynb 
+Not all the images have captions, hence used [BLIP](https://huggingface.co/Salesforce/blip-image-captioning-base) to generate captions to fill the gap. 
+Finally all the descriptions provided in IKEA datset and generated captions are merged together to form initial set of captions using the method /data_preprocessing/generation.py
+
+Initail captions are saved at dtaa/annotations/annotations_ikea.json
--- data_preprocessing/generation.py
+++ data_preprocessing/generation.py
@@ -6,14 +6,14 @@ import torch
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from alive_progress import alive_bar
 import time
-os.chdir('/Users/samankhan/Documents/capstone_project/Github_project/generate_ai_room_designs/')
+os.chdir(os.getenv("PROJECT_ROOT_PATH"))
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to(device)
-objects_extraction_dir = './data/anas/'
+objects_extraction_dir = './data/images/'
 annotation_path = './data/extracted_data/ikea_processed_data/'
 
 def generate_caption(image_path):
@@ -21,7 +21,7 @@ def generate_caption(image_path):
     inputs = processor(raw_image, return_tensors="pt").to(device)
 
     generation_args = {
-    "max_length": 100,  # Maximum length of the caption
+    "max_length": 500,  # Maximum length of the caption
     "num_beams": 5,    # Beam search with 5 beams
     "temperature": 1.0, # Sampling temperature
     "top_k": 50,       # Top-k sampling
@@ -36,17 +36,16 @@ def generate_caption(image_path):
 annotations = []
 
 for root, dirs, files in os.walk(objects_extraction_dir):
-    with alive_bar(len(os.listdir(files))) as bar:
-        for file in files:
-            if file.lower().endswith(('png', 'jpg', 'jpeg')):
-                image_path = os.path.join(root, file)
-                caption = generate_caption(image_path)
-                annotations.append({"file_name": file, "desc": caption})
-                time.sleep(.001)
-                bar()
+    for file in files:
+        if file.lower().endswith(('png', 'jpg', 'jpeg')):
+            image_path = os.path.join(root, file)
+            caption = generate_caption(image_path)
+            annotations.append({"file_name": file, "desc": caption})
+            # time.sleep(.001)
+            # bar()
 
-# annotations_file_path = os.path.join(annotation_path, 'annotations.json')
-# with open(annotations_file_path, 'w') as annotations_file:
-#     json.dump(annotations, annotations_file, indent=4)
+annotations_file_path = os.path.join(annotation_path, 'annotations_trial.json')
+with open(annotations_file_path, 'w') as annotations_file:
+    json.dump(annotations, annotations_file, indent=4)
 
 print(f"Annotations file created at: {annotations_file_path}")
