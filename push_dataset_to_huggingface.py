18c657faa131638cd80f3c1d3a2e647320ed9cf1
SamanK <saman.khan.stats@gmail.com>
2024-07-01 02:08:53 +0530

    updated paths to include environment vbl

--- push_dataset_to_huggingface.py
+++ push_dataset_to_huggingface.py
@@ -2,12 +2,13 @@ import os
 from datasets import load_dataset
 
 # Set this environment variable in you local
-project_root_path = os.environ["PROJECT_ROOT_PATH"]
+project_root_path = os.getenv("PROJECT_ROOT_PATH")
+hugging_face_user = os.getenv("HUGGING_FACE_USERNAME")
 
-dataset = load_dataset(project_root_path + '/create_dataset.py')
+dataset = load_dataset(project_root_path + '/create_dataset.py',project_root_path+'datasets/train/annotations/annotations.json')
 
 #change this to your HF Org 
-dataset.push_to_hub("nbadrinath/ikea_dataset_4.0")
+dataset.push_to_hub(hugging_face_user + "/ikea_dataset_2.0")
 # print(dataset["train"][0])
 # print(dataset["test"][0])
 # print(dataset["validation"][0])
