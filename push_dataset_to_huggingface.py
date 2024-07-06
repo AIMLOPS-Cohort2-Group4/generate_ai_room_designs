70b731a93f4dc885ce36f6d3d7910561bba6c373
SamanK <saman.khan.stats@gmail.com>
2024-07-07 00:05:09 +0530

    added method to read env files

--- push_dataset_to_huggingface.py
+++ push_dataset_to_huggingface.py
@@ -1,5 +1,8 @@
 import os
 from datasets import load_dataset
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Set this environment variable in you local
 project_root_path = os.getenv("PROJECT_ROOT_PATH")
@@ -12,7 +15,7 @@ validation_annotations_file_path = project_root_path + "/datasets/val/annotation
 data_files = {"train":train_annotations_file_path, "test":test_annotations_file_path, "validation":validation_annotations_file_path}
 dataset = load_dataset(project_root_path + '/create_dataset.py', data_files= data_files)
 
-dataset.push_to_hub(hugging_face_user + "/ikea_dataset_5.0")
+dataset.push_to_hub(hugging_face_user + "ikea_dataset_3.0")
 # print(dataset["train"][0])
 # print(dataset["test"][0])
 # print(dataset["validation"][0])
